402f58c39052700d879a500f7fde58d653567604
boyangmatsapola <boyangbass@gmail.com>
2019-11-20 10:12:15 -0600

    changed projects

--- abantudev/src/projectlist.js
+++ abantudev/src/projectlist.js
@@ -2,8 +2,8 @@ export const projectList = [
     {title : 'Giggrab',
     image: 'http://career.iresearchnet.com/wp-content/uploads/2015/05/Musician-1.jpg',
     description: ' A place where musicians can come together  to post and discover gigs.',
-    link: 'https://giggrab.herokuapp.com/',
-    github: 'https://github.com/boyangm/GigGrab'
+    link: 'https://merngiggrab.herokuapp.com/home',
+    github: 'https://github.com/boyangm/GigGrabv2.0'
     },
     {title : 'Daily Dally',
     image: 'https://media.wsls.com/photo/2017/04/24/Whats%20News%20Today_1493062809311_9576980_ver1.0_1280_720.png',
@@ -11,11 +11,11 @@ export const projectList = [
     link: 'https://dailydally.herokuapp.com/',
     github: 'https://github.com/boyangm/Newssite'
     },
-    {title : 'Burger Bar',
-    image: 'https://assets.bonappetit.com/photos/5d1cb1880813410008e914fc/16:9/w_1200,c_limit/Print-Summer-Smash-Burger.jpg',
-    description: 'Full Stack Web App mimicking a to- do list using SQL.',
-    link: 'https://boyangeataburger.herokuapp.com/',
-    github: 'https://github.com/boyangm/burger'
+    {title : 'Clicky Game',
+    image: 'https://www.ixxiyourworld.com/media/1676571/Mickey-Mouse-2.jpg?mode=crop&width=130&height=130',
+    description: 'React App that allows you practice your memory.',
+    link: 'https://boyangclicky.herokuapp.com/',
+    github: 'https://github.com/boyangm/clicky-game'
     },
     {title : 'Friend Finder',
     image: 'https://resources.christianquestions.com/wp-content/uploads/2018/09/05185336/dreamstime_m_91084934.jpg',
