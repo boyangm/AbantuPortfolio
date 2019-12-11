d2cbd8a824fc432c2fc544ea79bf949df4384548
boyangmatsapola <boyangbass@gmail.com>
2019-12-11 01:03:57 -0600

    download link works

--- abantudev/src/resume.js
+++ abantudev/src/resume.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import BoyangRes from '../src/assets/BoyangResume.pdf'
 const Resume = () => {
 
     return (
        <div className = 'downloadCont'>
-        <Link className='subHeading resume' to="/files/myfile.pdf" target="_blank" download>Download my Resume</Link>
+        <Link className='subHeading resume' to= {BoyangRes} target="_blank" download>Download my Resume</Link>
        </div>
     )
 }
