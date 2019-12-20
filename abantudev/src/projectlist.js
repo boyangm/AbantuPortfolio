1f0dab0dad0cda58fa175e13591699d5263d534a
boyangmatsapola <boyangbass@gmail.com>
2019-12-20 11:57:41 -0600

    project add

--- abantudev/src/projectlist.js
+++ abantudev/src/projectlist.js
@@ -6,28 +6,16 @@ export const projectList = [
     link: 'https://merngiggrab.herokuapp.com/home',
     github: 'https://github.com/boyangm/GigGrabv2.0'
     },
-    {title : 'Racr',
-    image: 'https://cdn.vox-cdn.com/thumbor/KO_gWQtcDEpfVrmVFuJnb9EYHo0=/0x40:1000x790/1200x800/filters:focal(0x40:1000x790)/cdn.vox-cdn.com/uploads/chorus_image/image/46889290/shutterstock_266758136.0.0.jpg',
-    description: 'React Web App to Allow Runners to race One Another',
-    link: 'https://mernracr.herokuapp.com/',
-    github: 'https://github.com/MissWibbon/racr'
-    },
     {title : 'PalletSound',
     image: AltRoomA,
     description: 'Freelance Website for Brideport Music Studio',
     link: 'http://www.palletsound.com',
     github: 'https://github.com/boyangm/PalletSound'
     },
-    {title : 'Clicky Game',
-    image: 'https://www.ixxiyourworld.com/media/1676571/Mickey-Mouse-2.jpg?mode=crop&width=130&height=130',
-    description: 'React App that allows you practice your memory.',
-    link: 'https://boyangclicky.herokuapp.com/',
-    github: 'https://github.com/boyangm/clicky-game'
-    },
-    {title : 'Friend Finder',
-    image: 'https://resources.christianquestions.com/wp-content/uploads/2018/09/05185336/dreamstime_m_91084934.jpg',
-    description: 'Friend Finder is a Mock Dating App using SQL and Express',
-    link: 'https://boyangfriendsforlife.herokuapp.com/',
-    github: 'https://github.com/boyangm/Friend-Finder'
+    {title : 'Racr',
+    image: 'https://cdn.vox-cdn.com/thumbor/KO_gWQtcDEpfVrmVFuJnb9EYHo0=/0x40:1000x790/1200x800/filters:focal(0x40:1000x790)/cdn.vox-cdn.com/uploads/chorus_image/image/46889290/shutterstock_266758136.0.0.jpg',
+    description: 'React Web App to Allow Runners to race One Another',
+    link: 'https://mernracr.herokuapp.com/',
+    github: 'https://github.com/MissWibbon/racr'
     }
 ]
