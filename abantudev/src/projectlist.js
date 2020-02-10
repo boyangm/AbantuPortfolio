b49110eda9b58abc49b2f8b2cea4fddfab77b934
boyangmatsapola <boyangbass@gmail.com>
2020-02-10 14:18:45 -0600

    changed projects

--- abantudev/src/projectlist.js
+++ abantudev/src/projectlist.js
@@ -2,15 +2,21 @@ import AltRoomA from '../src/assets/AltRoomA.JPG'
 export const projectList = [
     {title : 'Giggrab',
     image: 'http://career.iresearchnet.com/wp-content/uploads/2015/05/Musician-1.jpg',
-    description: ' A place where musicians can come together  to post and discover gigs.',
+    description: ' A React Web App where musicians can come together  to post and discover gigs.',
     link: 'https://merngiggrab.herokuapp.com/home',
     github: 'https://github.com/boyangm/GigGrabv2.0'
     },
     {title : 'PalletSound',
     image: AltRoomA,
-    description: 'Freelance Website for Brideport Music Studio',
+    description: 'Freelance React Website for Brideport Music Studio',
     link: 'http://www.palletsound.com',
     github: 'https://github.com/boyangm/PalletSound'
+    },
+    {title : 'Gem Tree Music',
+    image: 'https://gemtreemedia.s3.us-east-2.amazonaws.com/gemphotos/gem-tree-mums-cover-art-web%400.5x.JPG',
+    description: 'Freelance React Website for Recording Artist Gem Tree',
+    link: 'http://gemtree-website.s3-website.us-east-2.amazonaws.com/',
+    github: 'https://github.com/boyangm/GemTreeSite'
     },
     {title : 'Racr',
     image: 'https://cdn.vox-cdn.com/thumbor/KO_gWQtcDEpfVrmVFuJnb9EYHo0=/0x40:1000x790/1200x800/filters:focal(0x40:1000x790)/cdn.vox-cdn.com/uploads/chorus_image/image/46889290/shutterstock_266758136.0.0.jpg',
